2bbc9c5e45a1d6ca77003ccd807efa3182ede318
macben-debug <duyilmeitemiloluwa@gmail.com>
2026-05-17 04:14:04 +0100

    not sure about the save button yet

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -37,9 +37,12 @@ cart.forEach((cartItem)=>{
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link"
+            data-product-id="${matchingProduct.id}">
               Update
             </span>
+            <input class="quantity-input js-new-quantity-value-${matchingProduct}" >
+            <span class="save-quantity-link link-primary js-save-link" data-product-id ="${matchingProduct.id}">Save</span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
@@ -124,3 +127,27 @@ function updateCartQuantity (){
   }
 
 updateCartQuantity ();
+
+document.querySelectorAll(`.js-update-link`)
+  .forEach((link)=>{
+    link.addEventListener('click',()=>{
+      const productId = link.dataset.productId;
+      const container =document.querySelector(`.js-cart-item-container-${productId}`);
+      container.classList.add('is-editing-quantity');
+    });
+});
+
+document.querySelectorAll(`.js-save-link`)
+  .forEach((link)=>{
+    link.addEventListener('click',()=>{
+      const productId =link.dataset.productId;
+      const container =document.querySelector(`.js-cart-item-container-${productId}`);
+      container.classList.remove('is-editing-quantity');
+      
+      
+      const quantityInput= document.querySelector(
+        `.js-new-quantity-value-${productId}`
+      );
+      const newQuantity =Number( quantityInput);
+    })
+  })
