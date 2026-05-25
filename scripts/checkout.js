80ee9c26bb44d7d6cac3b759482b54d76a2b6594
macben-debug <duyilmeitemiloluwa@gmail.com>
2026-05-25 01:20:49 +0100

    checkout almost done

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -137,23 +137,26 @@ document.querySelectorAll(`.js-update-link`)
     });
 });
 
+
 document.querySelectorAll(`.js-save-link`)
   .forEach((link)=>{
     link.addEventListener('click',()=>{
       const productId =link.dataset.productId;
       const container =document.querySelector(`.js-cart-item-container-${productId}`);
       container.classList.remove('is-editing-quantity');
-      
-      
       const quantityInput= document.querySelector(
         `.js-quantity-input-${productId}`
       );
       const newQuantity = Number(quantityInput.value);
+      if(newQuantity <0|| newQuantity >=1000){
+        alert('Quantity must be atleast 0 and less than 1000');
+        return;
+      }
+      
       updateQuantity(productId,newQuantity);
       
-
-      const newValue = document.querySelector('.js-quantity-label').innerHTML= newQuantity;
-
-
+       document.querySelector('.js-quantity-label').innerHTML= newQuantity;
+      updateCartQuantity ();
     })
   });
+
