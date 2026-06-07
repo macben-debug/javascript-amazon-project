64938e40c74de27462907cb0d7afc0a5e77bde2e
macben-debug <duyilmeitemiloluwa@gmail.com>
2026-06-07 02:51:55 +0100

    save data and genrate HTML fro payment summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,9 +1,9 @@
 import { calculateCartQuantity, cart,removeFromCart,updateQuantity,updateDeliveryOption } from "../../data/cart.js";
-import { products } from "../../data/products.js";
+import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import  dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 
 
 export function renderOrderSummary(){
@@ -13,22 +13,12 @@ export function renderOrderSummary(){
   cart.forEach((cartItem)=>{
   const productId =cartItem.productId;
 
-  let matchingProduct;
-
-  products.forEach((product)=>{
-    if(product.id === productId){
-      matchingProduct =product;
-    }
-  });
-
+  const  matchingProduct =getProduct(productId);
   const deliveryOptionId= cartItem.deliveryOptionId;
 
-  let deliveryOption;
-  deliveryOptions.forEach((option)=>{
-    if (option.id === deliveryOptionId){
-      deliveryOption = option
-    }
-  });
+  let deliveryOption= getDeliveryOption(deliveryOptionId);
+
+
   const today = dayjs();
     const deliveryDate = today.add(
       deliveryOption.deliveryDays,
