7bda022ade0c7177218daca9ef5eb57ef5198d23
macben-debug <duyilmeitemiloluwa@gmail.com>
2026-07-03 14:30:46 +0100

    order summary items sorted

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -23,6 +23,10 @@ export function renderPaymentSummary(){
   const taxCents =totalBeforeTax *0.1;
   const  totalCents = totalBeforeTax +taxCents;
 
+  let cartQuantity= 0 ;
+  cart.forEach((cartItem)=>{
+    cartQuantity += cartItem.quantity;
+  });
 
   const paymentSummaryHTML= `
     <div class="payment-summary-title">
@@ -30,7 +34,7 @@ export function renderPaymentSummary(){
     </div>
 
     <div class="payment-summary-row">
-      <div>Items <div class = "js-cart-quantity"></div>:</div>
+      <div>Items (${cartQuantity}):</div>
       <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
     </div>
 
